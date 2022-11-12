a09383fd0acf800599afdfb2a336e9a77a9810a6
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-11-12 18:40:32 -0300

    everyfreaking day

--- script.js
+++ script.js
@@ -12,3 +12,9 @@ console.log('testing')
 setTimeout(() => {
 	console.log('depois de 1s, todo santo dia eu vou ao menos abrir isso e escrever upar algo xD')
 }, 1000)
+
+function everythingWillBeOk(withWhom) {
+	console.log(withWhom)
+}
+
+everythingWillBeOk('with me')
